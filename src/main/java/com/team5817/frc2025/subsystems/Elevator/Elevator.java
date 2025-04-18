3842e08cb371e717be88014de4f9e5fd2ebec090
Hubert Hsu <sangerroboticsprogramming@gmail.com>
2025-04-18 15:53:17 -0700

    Update at 'Fri Apr 18 15:53:17 PDT 2025'

--- src/main/java/com/team5817/frc2025/subsystems/Elevator/Elevator.java
+++ src/main/java/com/team5817/frc2025/subsystems/Elevator/Elevator.java
@@ -49,7 +49,7 @@ public class Elevator extends ServoMotorSubsystem {
 	 */
 	public enum State {
 		L4(1.8635330123363545, kStrictError, ElevatorConstants.kHighOffsetMap),
-		L3(1.211806509200769-.06, kStrictError, ElevatorConstants.kMidOffsetMap),
+		L3(1.211806509200769-.045, kStrictError, ElevatorConstants.kMidOffsetMap),
 		L2A(.754804-.01, kStrictError, ElevatorConstants.kMidOffsetMap),
 		L2(.754804-.02, kStrictError, ElevatorConstants.kMidOffsetMap),
 		L1(0.219, kStrictError),
